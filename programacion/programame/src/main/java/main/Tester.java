e4d2a5b3e27fb76ad01fd0bd2167096082af785b
oscar.novillo <oscar.novillo@gmail.com>
2019-10-23 00:21:03 +0200

    dfsdf

--- programacion/programame/src/main/java/main/Tester.java
+++ programacion/programame/src/main/java/main/Tester.java
@@ -5,30 +5,34 @@
  */
 package main;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
 /**
- *
  * @author oscar
  */
 public class Tester {
 
   public static void main(String[] args) throws IOException {
-     String entrada = "3 2\n" +
-"1 2 2 5 2 4\n" +
-"2 1 3 3\n" +
-"1 1\n" +
-"1000000 40\n" +
-"1000000 40";
-    
-   final ByteArrayInputStream input 
-		= new ByteArrayInputStream(entrada.getBytes());
-		System.setIn(input);
-    
+//    String entrada = "3 2\n" +
+//        "1 2 2 5 2 4\n" +
+//        "2 1 3 3\n" +
+      String entrada =   "2 3\n" +
+        "10 1 1 1\n" +
+        "2 1 8 2 1 1";
+
+    final ByteArrayInputStream input
+        = new ByteArrayInputStream(entrada.getBytes());
+    System.setIn(input);
+    LocalDateTime antes = LocalDateTime.now();
     Houston511.main(args);
+    LocalDateTime despues = LocalDateTime.now();
+    System.out.println();
+    System.out.println(Duration.between(antes,despues).toMillis());
   }
 
 }
