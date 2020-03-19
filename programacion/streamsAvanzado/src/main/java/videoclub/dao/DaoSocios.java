810755ecdca881ca29595832e84fab1283db50f9
oscar.novillo <oscar.novillo@gmail.com>
2020-03-20 00:34:39 +0100

    frere

---
+++ programacion/streamsAvanzado/src/main/java/videoclub/dao/DaoSocios.java
@@ -0,0 +1,38 @@
+package videoclub.dao;
+
+import videoclub.dao.modelo.Socio;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DaoSocios {
+
+    private static Map<String, Socio> socios = new HashMap<>();
+
+    // devuelve si se metio o no
+    public boolean addSocio(Socio socio) {
+        boolean insertado = false;
+        if (socios.get(socio.getNif()) == null) {
+            DaoSocios.socios.put(socio.getNif(), socio);
+            insertado = true;
+        }
+
+        return insertado;
+    }
+
+
+
+    public Socio getSocioPorNif(String nif) {
+        return socios.get(nif);
+    }
+
+    public boolean deleteSocio(Socio socio) {
+        boolean borrado = false;
+        if (socios.containsKey(socio.getNif())) {
+            DaoSocios.socios.remove(socio.getNif());
+            borrado = true;
+        }
+        return borrado;
+    }
+
+}
