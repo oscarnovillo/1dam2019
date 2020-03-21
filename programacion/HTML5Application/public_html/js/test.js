53f06b2ec6d6c3c922c803f3a1b205bd00794ab0
oscar.novillo <oscar.novillo@gmail.com>
2020-03-21 13:46:10 +0100

    Merge origin/master
    Conflicts:
    	CarcasaJavaFX/nbactions.xml
    	CarcasaJavaFX/pom.xml

---
+++ programacion/HTML5Application/public_html/js/test.js
@@ -0,0 +1,25 @@
+var test = ",,,";
+test += ",,,kk";
+test = 9;
+test += 10;
+
+
+function sacaMensaje(mensaje,numero)
+{
+    numero.veces= 10;
+    var k = 90;
+    
+    alert(mensaje);
+    
+    alert(document.getElementById("textPrueba").value);
+    document.getElementById("textPrueba").value = "hhh";
+    document.getElementById("texto").innerHTML = "hola emily";
+}
+
+
+function otraFuncion()
+{
+    return "saludos";
+}
+
+
