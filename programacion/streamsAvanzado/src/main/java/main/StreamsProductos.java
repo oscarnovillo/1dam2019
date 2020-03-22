50ebfc23e1c9400683dbb5bd1a95b829afe86fa7
oscar.novillo <oscar.novillo@gmail.com>
2020-03-23 00:40:08 +0100

    frere

--- programacion/streamsAvanzado/src/main/java/main/StreamsProductos.java
+++ programacion/streamsAvanzado/src/main/java/main/StreamsProductos.java
@@ -1,4 +1,26 @@
 package main;
 
 public class StreamsProductos {
+
+  // con reduce y con sorted
+  public void productoMasCaro() {
+  }
+
+  //con reduce y con sorted
+  public void productoMasBarato() {
+  }
+
+
+  public void mediaPrecioTodosLosProductos() {
+  }
+
+
+  // un grupo de producto por cada franja de 10, de 0 a 10, 11 a 20, etc...
+  public void productosAgrupadosPorRangoPrecio10en10() {
+  }
+
+  // de los productos que tenga precio de 11 a 20, indicar cuales tienen stock mayor que 5
+  public void productosConPrecio11a20YStockMayor5() {
+  }
+
 }
--- programacion/streamsAvanzado/src/main/java/main/StreamsVideoClub.java
+++ programacion/streamsAvanzado/src/main/java/main/StreamsVideoClub.java
@@ -1,4 +1,34 @@
 package main;
 
 public class StreamsVideoClub {
+
+  public void numeroSociosSancionados(){}
+
+  public void mediaEdadDeSociosSancionados(){}
+
+  public void listaDiezProductosMasAlquilados(){}
+
+  // numero de Peliculas Alquiladas, de Documentales y Videojuegos.
+  public void numeroProductosAlquiladosPorTipo(){}
+
+  public void todosLosActoresDistintosDeTodasLasPeliculas() {}
+
+  public void peliculaConMasActores(){}
+
+
+  //el listado de productos ordenados de mayor a menor según su valoración media.
+  public void productoConSuValoracionMediaOrdenadosDeMayoraMenor(){}
+
+  public void las10PeliculasMejorValoradas(){}
+
+  public void los10VideoJuegosMejorValoradas(){}
+
+
+  // el numero de DVD y Videos que hay.
+  public void numeroDocumentalesyPeliculasSegunSuFormato(){}
+
+  // conseguir un String con todos los faricantes distintos de videojuegos separados por ,
+  public void todosLosFabricantesDistintosDeVideoJuegosEnUnSoloString(){}
+
+
 }
