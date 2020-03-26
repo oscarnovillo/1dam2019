7bc14409362f21f3dd99efd67d3c4e3185358ec0
oscar.novillo <oscar.novillo@gmail.com>
2020-03-26 18:16:01 +0100

    frere

--- programacion/streamsAvanzado/src/main/java/main/StreamsPedidos.java
+++ programacion/streamsAvanzado/src/main/java/main/StreamsPedidos.java
@@ -1,4 +1,24 @@
 package main;
 
 public class StreamsPedidos {
+
+
+
+
+  // un map con nombre de producto y cantidad de veces pedido
+  public void productosAgrupadosPorCantidadDeVecesPedidos(){}
+
+  public void clienteQueMasDineroSehaGastado(){}
+
+
+  // La cantidad media de producto por pedido simple, sumando todas las lineas de pedido de cada simple
+  public void lacantidadMediaPedidaDeCadaProductoEnCadaPedidoCompuesto(){}
+
+
+  public void pedidoSimpleConMasLineasdePedido() {}
+
+
+  public void todoelDineroFacturadoEnTotalentodosLosPedidos(){}
+
+
 }
