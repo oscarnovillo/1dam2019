90fbf5dec8729adfae585a658d849ecdc2c2e22d
oscar.novillo <oscar.novillo@gmail.com>
2020-04-03 19:03:53 +0200

    sdfsdf

---
+++ programacion/videoclub_Esther/src/main/java/dao/modelo/Producto.java
@@ -0,0 +1,103 @@
+package dao.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class Producto {
+
+    private String titulo;
+    private List<Encuesta> encuestas;
+    private int cantidad;
+    private int cantidadAlquilada;
+    private String genero;
+    private double valoracionMedia;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Producto producto = (Producto) o;
+        return getCantidad() == producto.getCantidad() &&
+            getCantidadAlquilada() == producto.getCantidadAlquilada() &&
+            Objects.equals(getTitulo(), producto.getTitulo()) &&
+            Objects.equals(getGenero(), producto.getGenero());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitulo(), getCantidad(), getCantidadAlquilada(), getGenero());
+    }
+
+    public Producto(String titulo, int cantidad, String genero) {
+        this();
+        this.titulo = titulo;
+        this.cantidad = cantidad;
+        this.genero = genero;
+    }
+
+    public Producto() {
+        encuestas = new ArrayList<>();
+        this.cantidadAlquilada = 0;
+        this.valoracionMedia = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "titulo='" + titulo + '\'' +
+                ", cantidad=" + cantidad +
+                ", cantidadAlquilada=" + cantidadAlquilada +
+                ", cantidadDisponible=" + (cantidad - cantidadAlquilada) +
+                ", genero='" + genero;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Encuesta> getEncuestas() {
+        return encuestas;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidadAlquilada() {
+        return cantidadAlquilada;
+    }
+
+    public void setCantidadAlquilada(int cantidadAlquilada) {
+        this.cantidadAlquilada = cantidadAlquilada;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getValoracionMedia() {
+        return valoracionMedia;
+    }
+
+    public void setValoracionMedia(double valoracionMedia) {
+        this.valoracionMedia = valoracionMedia;
+    }
+}
